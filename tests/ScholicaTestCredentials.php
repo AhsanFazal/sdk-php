0285db6ae471023c7c5dd4c6270f4a81d9ef1a89
Thomas Schoffelen <tom@scholica.com>
2014-10-21 22:26:41 +0200

    Updating structure preparing for 1.1

---
+++ tests/ScholicaTestCredentials.php
@@ -0,0 +1,11 @@
+<?php
+
+class ScholicaTestCredentials {
+
+    /**
+     * These must be filled out with valid Scholica app details for the tests to run.
+     */
+    public static $consumer_key = '';
+    public static $consumer_secret = '';
+
+}
