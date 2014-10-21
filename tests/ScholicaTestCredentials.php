a0745986a2aa0d0f7f3c1d1306cfdc13429e874c
Thomas Schoffelen <tom@scholica.com>
2014-10-21 23:50:25 +0200

    Fixed testing bootstrap

--- tests/ScholicaTestCredentials.php
+++ tests/ScholicaTestCredentials.php
@@ -5,7 +5,8 @@ class ScholicaTestCredentials {
     /**
      * These must be filled out with valid Scholica app details for the tests to run.
      */
-    public static $consumer_key = '';
-    public static $consumer_secret = '';
+    public static $consumer_key = 'Vm0weE1HRXdOVWRXV0d4VVYwZDRWRmx0ZEhkVU1WcHpWMjFHVjJKR2JETlhhMUpU';
+    public static $consumer_secret = 'mlSMmhXVm14a2IxSkdjRWhsUjBaVVVsUldXbGRyWkc5VWJVVjRZMFZvVjFKc2NGaFdha1poVWpGa2NsZHNVbWxTVlhCdlZtM';
+    public static $access_token = '';
 
 }
